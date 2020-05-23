55ac5a41445623c82afca0e574f00e9cc9e6110d
sas94086 <sandra_seymour@hotmail.com>
2020-05-22 22:41:50 -0700

    This is a python file that I will use for my own build pipeline in Buildkite
    this file calculates the area given inputs, I have hardcoded the values rather than request it

---
+++ testareacalc.py
@@ -0,0 +1,23 @@
+# An example to find area of a triangle using a version of Heron's formula
+import math
+
+def calcarea(a,b,c):
+	# The three arguments are typecast, as they are initially strings
+	Area = area_triangle(a,b,c)
+	print("The area is:  %f" % Area)
+	
+	
+# Calculate the area 	
+def area_triangle(a,b,c):
+    A = (1.0/4.0) * math.sqrt(
+        2*(a**2 * b**2 + a**2 * c**2 + b**2 * c**2) -
+        (a**4 + b**4 + c**4) )
+    return A
+ 
+if __name__=='__main__':
+	#The arguments come from the user
+	a = 8.0
+	b = 8.0 
+	c = 8.0 
+	calcarea(a,b,c)  
+
